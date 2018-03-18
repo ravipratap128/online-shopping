9cc396ac39eca6dc1608303ab8bc08bce435c977
ravi <ravipratap128@gmail.com>
2018-03-18 13:08:15 +0530

    First setup stage completed

---
+++ shoppingbackend/src/main/java/com/cb/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.cb.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
