5a58bc2865908b52c8afc0e3f00d3163bf7c3de7
ravi <ravipratap128@gmail.com>
2018-03-23 00:29:23 +0530

    successfully commited static resources

---
+++ onlineshopping/src/main/java/com/cb/onlineshopping/controller/PageController.java
@@ -0,0 +1,35 @@
+package com.cb.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+@Controller
+public class PageController {
+	@RequestMapping(value = {"/home", "/index"})
+	public ModelAndView index()
+	{
+		ModelAndView mv = new ModelAndView("page");
+			mv.addObject("msg","welcome to spring mvc");
+			return mv;
+	}
+	
+	
+	
+/*	@RequestMapping(value = {"/test"})
+	public ModelAndView test(@RequestParam(value="messege",required=false) String messege)
+	{
+		ModelAndView mv = new ModelAndView("page");
+			mv.addObject("msg",messege);
+			return mv;
+	}
+	
+	@RequestMapping(value = {"/test/{messege}"})
+	public ModelAndView test1(@PathVariable("messege") String messege)
+	{
+		ModelAndView mv = new ModelAndView("page");
+			mv.addObject("msg",messege);
+			return mv;
+	}*/
+	
+
+}
